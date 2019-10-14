39b014e134b9ae587dd10c01de8df78946a049ab
Dougllas Sousa <dougllasfps@gmail.com>
2019-10-13 21:49:58 -0300

    implementacao dos testes de integracao de lancamentos

---
+++ src/test/java/com/dsousa/minhasfinancas/model/repository/LancamentoRepositoryTest.java
@@ -0,0 +1,106 @@
+package com.dsousa.minhasfinancas.model.repository;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase.Replace;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import com.dsousa.minhasfinancas.model.entity.Lancamento;
+import com.dsousa.minhasfinancas.model.enums.StatusLancamento;
+import com.dsousa.minhasfinancas.model.enums.TipoLancamento;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+import java.util.Optional;
+
+import static org.assertj.core.api.Assertions.*;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+@RunWith(SpringRunner.class)
+@DataJpaTest
+@AutoConfigureTestDatabase(replace = Replace.NONE)
+@ActiveProfiles("test")
+public class LancamentoRepositoryTest {
+
+	@Autowired
+	LancamentoRepository repository;
+	
+	@Autowired
+	TestEntityManager entityManager;
+	
+	@Test
+	public void deveSalvarUmLancamento() {
+		Lancamento lancamento = criarLancamento();
+		
+		lancamento = repository.save(lancamento);
+		
+		assertThat(lancamento.getId()).isNotNull();
+	}
+
+	@Test
+	public void deveDeletarUmLancamento() {
+		Lancamento lancamento = criarEPersistirUmLancamento();
+		
+		lancamento = entityManager.find(Lancamento.class, lancamento.getId());
+		
+		repository.delete(lancamento);
+		
+		Lancamento lancamentoInexistente = entityManager.find(Lancamento.class, lancamento.getId());
+		assertThat(lancamentoInexistente).isNull();
+	}
+
+	
+	@Test
+	public void deveAtualizarUmLancamento() {
+		Lancamento lancamento = criarEPersistirUmLancamento();
+		
+		lancamento.setAno(2018);
+		lancamento.setDescricao("Teste Atualizar");
+		lancamento.setStatus(StatusLancamento.CANCELADO);
+		
+		repository.save(lancamento);
+		
+		Lancamento lancamentoAtualizado = entityManager.find(Lancamento.class, lancamento.getId());
+		
+		assertThat(lancamentoAtualizado.getAno()).isEqualTo(2018);
+		assertThat(lancamentoAtualizado.getDescricao()).isEqualTo("Teste Atualizar");
+		assertThat(lancamentoAtualizado.getStatus()).isEqualTo(StatusLancamento.CANCELADO);
+	}
+	
+	@Test
+	public void deveBuscarUmLancamentoPorId() {
+		Lancamento lancamento = criarEPersistirUmLancamento();
+		
+		Optional<Lancamento> lancamentoEncontrado = repository.findById(lancamento.getId());
+		
+		assertThat(lancamentoEncontrado.isPresent()).isTrue();
+	}
+
+	private Lancamento criarEPersistirUmLancamento() {
+		Lancamento lancamento = criarLancamento();
+		entityManager.persist(lancamento);
+		return lancamento;
+	}
+	
+	private Lancamento criarLancamento() {
+		return Lancamento.builder()
+									.ano(2019)
+									.mes(1)
+									.descricao("lancamento qualquer")
+									.valor(BigDecimal.valueOf(10))
+									.tipo(TipoLancamento.RECEITA)
+									.status(StatusLancamento.PENDENTE)
+									.dataCadastro(LocalDate.now())
+									.build();
+	}
+	
+	
+	
+	
+	
+}
--- src/test/java/com/dsousa/minhasfinancas/service/UsuarioServiceTest.java
+++ src/test/java/com/dsousa/minhasfinancas/service/UsuarioServiceTest.java
@@ -3,14 +3,9 @@ package com.dsousa.minhasfinancas.service;
 import java.util.Optional;
 
 import org.assertj.core.api.Assertions;
-import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.mockito.Mock;
 import org.mockito.Mockito;
-import org.mockito.MockitoAnnotations;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.boot.test.mock.mockito.SpyBean;
 import org.springframework.test.context.ActiveProfiles;
