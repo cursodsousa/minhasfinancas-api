c484fd478149fe9c1b3885178a07da47a269ff3d
Dougllas Sousa <dougllasfps@gmail.com>
2019-10-19 23:21:12 -0300

    adicionando configuracoes para conexão com o frontend

--- src/main/java/com/dsousa/minhasfinancas/MinhasfinancasApplication.java
+++ src/main/java/com/dsousa/minhasfinancas/MinhasfinancasApplication.java
@@ -2,9 +2,18 @@ package com.dsousa.minhasfinancas;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
-public class MinhasfinancasApplication {
+@EnableWebMvc
+public class MinhasfinancasApplication implements WebMvcConfigurer {
+	
+	@Override
+	public void addCorsMappings( CorsRegistry registry ) {
+		registry.addMapping("/**").allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS");
+	}
 	
 	public static void main(String[] args) {
 		SpringApplication.run(MinhasfinancasApplication.class, args);
--- src/main/java/com/dsousa/minhasfinancas/api/dto/UsuarioDTO.java
+++ src/main/java/com/dsousa/minhasfinancas/api/dto/UsuarioDTO.java
@@ -1,12 +1,16 @@
 package com.dsousa.minhasfinancas.api.dto;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Getter
 @Setter
 @Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class UsuarioDTO {
 
 	private String email;
